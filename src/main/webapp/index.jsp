ce1a80854b057a62dd96211c1e85b2a1fb2fb9a4
Nikita Kuchur <nikitakuchur@gmail.com>
2020-03-08 22:08:46 +0300

    Initial commit

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>web-board</title>
+        <link rel="stylesheet" type="text/css" href="css/board.css">
+    </head>
+    <body>
+        <canvas id="draw"></canvas>
+        <button id="clear-button">Clear</button>
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/draw.js"></script>
+    </body>
+</html>
---
+++ src/main/webapp/js/draw.js
@@ -0,0 +1,107 @@
+let canvas, ctx,
+    brush = {
+        color: '#000000',
+        size: 10,
+        down: false,
+    },
+    strokes = [],
+    currentStroke = null;
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width(), canvas.height());
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+    for (let stroke of strokes) {
+        ctx.strokeStyle = stroke.color;
+        ctx.lineWidth = stroke.size;
+        ctx.beginPath();
+        ctx.moveTo(stroke.points[0].x, stroke.points[0].y);
+        for (let point of stroke.points) {
+            ctx.lineTo(point.x, point.y)
+        }
+        ctx.stroke();
+    }
+}
+
+function clear() {
+    strokes = [];
+    $.ajax({
+        type: 'POST',
+        url: 'board',
+        data: {clear: true},
+    });
+    draw();
+}
+
+function init() {
+    canvas = $('#draw');
+    ctx = canvas[0].getContext('2d');
+
+    function updateSize() {
+        canvas.attr({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    }
+    $(window).resize(updateSize).resize();
+
+    (function refresher() {
+        $.ajax('board', {
+            success: function (data) {
+                if (!brush.down) {
+                    strokes = JSON.parse(data);
+                    draw();
+                }
+            },
+            complete: function () {
+                setTimeout(refresher, 400);
+            }
+        });
+    })();
+
+    function mouseEvent(e) {
+        if (currentStroke != null) {
+            currentStroke.points.push({
+                x: e.pageX,
+                y: e.pageY,
+            });
+            draw()
+        }
+    }
+
+    canvas.mousedown(function (e) {
+        brush.down = true;
+        currentStroke = {
+            color: brush.color,
+            size: brush.size,
+            points: [],
+        };
+        strokes.push(currentStroke);
+        mouseEvent(e);
+    });
+
+    canvas.mouseup(function (e) {
+        $.ajax({
+            type: 'POST',
+            url: 'board',
+            data: {stroke: JSON.stringify(currentStroke)},
+            dataType: 'json',
+            complete: function () {
+                brush.down = false;
+            }
+        });
+        currentStroke = null;
+    });
+
+    canvas.mousemove(function (e) {
+        if (brush.down) {
+            mouseEvent(e);
+        }
+    });
+
+    $('#clear-button').click(function (e) {
+        clear();
+    });
+}
+
+$(init);
