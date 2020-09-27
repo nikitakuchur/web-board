8edd400728787454c7f761478dfa3545fb10f26e
Nikita Kuchur <nikitakuchur@gmail.com>
2020-09-28 01:05:51 +0300

    Fixed SessionFactoryProducer

--- src/main/java/com/github/nikitakuchur/webboard/SessionFactoryProducer.java
+++ src/main/java/com/github/nikitakuchur/webboard/SessionFactoryProducer.java
@@ -11,25 +11,18 @@ import org.hibernate.cfg.Configuration;
 
 @ApplicationScoped
 public class SessionFactoryProducer {
-    private SessionFactory sessionFactory;
 
     private SessionFactoryProducer() {
     }
 
     @Produces
-    public SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration().configure();
-                configuration.addAnnotatedClass(Point.class);
-                configuration.addAnnotatedClass(Stroke.class);
-                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties());
-                sessionFactory = configuration.buildSessionFactory(builder.build());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return sessionFactory;
+    @ApplicationScoped
+    public static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().configure();
+        configuration.addAnnotatedClass(Point.class);
+        configuration.addAnnotatedClass(Stroke.class);
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties());
+        return configuration.buildSessionFactory(builder.build());
     }
 }
